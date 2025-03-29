ade34d119ead73ecbd4eb7327c3e60e9139557c4
Strkss <Strkss@users.noreply.github.com>
2025-03-29 09:27:13 +0700

    deleted unused file

--- PvZ2_Remake/Random.hpp
+++
@@ -1,8 +0,0 @@
-#include <random>
-#include <chrono>
-
-using std::mersenne_twister_engine;
-
-std::mt19937 rd(std::chrono::steady_clock::now().time_since_epoch().count());
-
-int Rand(int l, int r);
